b4b0076d8a33c92089562e454b998fff97bae695
fedeayala <fede121093@gmail.com>
2022-04-08 00:19:54 -0300

    Configurando la App

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Éste es mi primer <code>Hola Mundo en React!</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/FedeAyala"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mi Repositorio, FedeAyala!
         </a>
       </header>
     </div>
